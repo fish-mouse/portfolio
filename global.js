4b00c8a8a4c6afbadccb00550c9e2411a7637417
fish-mouse <rickyz11866@gmail.com>
2025-10-15 10:59:18 -0700

    fixing nav bar issue

--- global.js
+++ global.js
@@ -13,10 +13,10 @@ function $$(selector, context = document) {
 
 let pages = [
   { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
   { url: 'https://github.com/fish-mouse', title: 'Profile' },
-  { url: 'resume.html', title: 'Resume' },
-  { url: 'contact/', title: 'Contact' }
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume.html', title: 'Resume' }
 ];
 
 let nav = document.createElement('nav');
@@ -24,15 +24,35 @@ document.body.prepend(nav);
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
-  : "/https://fish-mouse.github.io/portfolio/";         // GitHub Pages repo name
+  : "fish-mouse.github.io/portfolio/";         // GitHub Pages repo name
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  url = !url.startsWith('fish') ? BASE_PATH + url : url;
 
-  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  console.log("a.host: ", a.host);
+  console.log("location.host: ", location.host);
+  console.log("a.pathname: ", a.pathname);
+  console.log("location.pathname: ", location.pathname);
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host === location.host && location.pathname === '/index.html' && a.pathname === '/' ) {
+    a.classList.add('current');
+  }
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
 }
 
 
