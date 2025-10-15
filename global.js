15a3bd12d3248a28f34a3f17d3ab4f0400e6e27f
fish-mouse <rickyz11866@gmail.com>
2025-10-14 22:58:07 -0700

    Adding Smart Nav system

---
+++ global.js
@@ -0,0 +1,36 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// navLinks = $$("nav a")
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'https://github.com/fish-mouse', title: 'Profile' },
+  { url: 'resume.html', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/fish-mouse.github.io/portfolio/";         // GitHub Pages repo name
+
+url = !url.startsWith('http') ? BASE_PATH + url : url;
+
