14837baf4362a7340ad166d52d3347c0b5a5dbea
fish-mouse <rickyz11866@gmail.com>
2025-10-23 15:04:25 -0700

    Saving current work (unchecked)

--- global.js
+++ global.js
@@ -77,5 +77,42 @@ select.addEventListener('input', function (event) {
   document.documentElement.style.setProperty('color-scheme', event.target.value);
 
   localStorage.colorScheme = event.target.value;
-
 });
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  containerElement.innerHTML = '';
+  for(let i = 0; i < projects.length; i++) {
+    project = projects[i];
+    const article = document.createElement('article');  
+    article.innerHTML = `
+      <h3>${project.title}</h3>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  }
+  
+}
+
+// console.log(fetchJSON('../lib/projects.json').data)
