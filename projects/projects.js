4772a53841a6257ef1509d45c5b672f19227b8bc
fish-mouse <rickyz11866@gmail.com>
2025-10-29 15:22:52 -0700

    Bringing the future of filtering

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,17 @@
 import {fetchJSON, renderProjects, renderProjCount} from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-const projects = await fetchJSON('../lib/projects.json');
+let projects = await fetchJSON('../lib/projects.json');
+
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 const projectsContainer = document.querySelector('.projects');
 
@@ -13,49 +23,6 @@ renderProjCount(projects.length, projTitleContainer);
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-// let arc = arcGenerator({
-//   startAngle: 0,
-//   endAngle: 2 * Math.PI,
-// });
-
-// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-
-// let data = [2, 1];
-
-// let total = 0;
-
-// for (let d of data) {
-//   total += d;
-// }
-
-// let angle = 0;
-// let arcData = [];
-
-// for (let d of data) {
-//   let endAngle = angle + (d / total) * 2 * Math.PI;
-//   arcData.push({ startAngle: angle, endAngle });
-//   angle = endAngle;
-// }
-
-// let arcs = arcData.map((d) => arcGenerator(d));
-
-// let colors = ['purple', 'yellow'];
-
-// arcs.forEach((arc, idx) => {
-//     d3.select('svg')
-//       .append('path')
-//       .attr('d', arc)
-//       .attr('fill', colors[idx]) // Fill in the attribute for fill color via indexing the colors variable
-// })
-
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
 let sliceGenerator = d3.pie().value((d) => d.value);
 
 let arcData = sliceGenerator(data);
@@ -76,4 +43,19 @@ data.forEach((d, idx) => {
     .append('li')
     .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+});
+
+
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('change', (event) => {
+  query = event.target.value;
+  let filteredProjects = projects.filter((project) =>{
+      let values = Object.values(project).join('\n').toLowerCase();
+      return values.includes(query.toLowerCase());
+    }
+  );
+
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+
 });
